faf8e262cde9a7ad6ee44424f18444bf4fe15143
pelowok <pelowok@users.noreply.github.com>
2024-02-01 15:10:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
 
 document.addEventListener('DOMContentLoaded', function() {
   var characters = [
