7ffcf890b5982bcff37805695b570970012c406c
pelowok <pelowok@users.noreply.github.com>
2024-02-08 16:42:08 -0500

    s

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $.getJSON( "/data.json", function( data ) {
     console.log(data);
-    $("#text").html(data["text"]);
+    $("#text").html(data["myText"]);
   });
 
   var characters = [
